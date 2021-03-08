3c1b3fe0e9a05eb475aa096f39b294e782107b07
orla11 <orlandif.mail@gmail.com>
2021-03-08 23:55:08 +0100

    modified index, moved html into different components

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+<mdb-container>
+    <Header />
+    <Form />
+    <Footer />
+</mdb-container>
+</template>
+
+<script>
+  import {mdbContainer} from 'mdbvue';
+  export default {
+    components: {
+      mdbContainer
+    }
+  }
+</script>
+
+<style>
+
+</style>
