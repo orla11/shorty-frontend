719246142fb62989138d87242406252f20d65111
orla11 <orlandif.mail@gmail.com>
2021-03-09 23:43:09 +0100

    created Loader component which is shown while axios fetches the shortened url

---
+++ components/Loader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="loader_center">
+        <div v-for="i in 8" v-bind:key="i" class="spinner-grow text-primary" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .loader_center {
+        text-align: center;
+    }
+</style>
